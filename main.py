c0323c95ffcd1b282b90728186e3c69e4ab096e7
Dimitri Glazkov <dimitri@glazkov.com>
2022-12-23 12:43:15 -0800

    Got the basics going.

---
+++ main.py
@@ -0,0 +1,97 @@
+import os
+import re
+import traceback
+
+
+from ask_embeddings import ask
+import openai
+from dotenv import load_dotenv
+from flask import Flask, jsonify, render_template, request
+
+START_QUERY = "list some interesting key concepts, each on new line"
+LIST_QUERY = "list some interesting key concepts related to {concept}, each on new line"
+DESCRIBE_QUERY = "describe {concept}"
+
+app = Flask(__name__)
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_TOKEN")
+
+def sanitize(line):
+    pattern = re.compile(r"^\d+\.")
+    return pattern.sub("", line).lstrip("* ").lstrip("- ").strip()
+
+    
+@app.route("/api/start", methods=["POST"])
+def start():
+    try:
+        response = ask(START_QUERY, "embeddings/what-dimitri-learned.pkl")
+        lines = [sanitize(line) for line in response.splitlines()]
+        return jsonify({
+            "list": lines
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": f"{e}\n{traceback.print_exc()}"
+        })
+
+
+@app.route("/api/start", methods=["GET"])
+def start_sample():
+    return render_template("start.html")
+
+
+@app.route("/api/describe", methods=["POST"])
+def describe():
+    concept = request.form["concept"]
+    if not concept:
+        return jsonify({
+            "error": "Concept is required"
+        })
+    try:
+        response = ask(DESCRIBE_QUERY.format(concept=concept),
+                       "embeddings/what-dimitri-learned.pkl")
+        return jsonify({
+            "text": response
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": f"{e}\n{traceback.print_exc()}"
+        })
+
+
+@app.route("/api/describe", methods=["GET"])
+def describe_sample():
+    return render_template("describe.html")
+
+
+@app.route("/api/list", methods=["POST"])
+def list():
+    concept = request.form["concept"]
+    if not concept:
+        return jsonify({
+            "error": "Concept is required"
+        })
+    try:
+        response = ask(LIST_QUERY.format(concept=concept),
+                       "embeddings/what-dimitri-learned.pkl")
+        lines = [sanitize(line) for line in response.splitlines()]
+        return jsonify({
+            "list": lines
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": f"{e}\n{traceback.print_exc()}"
+        })
+
+
+@app.route("/api/list", methods=["GET"])
+def list_sample():
+    return render_template("list.html")
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=8080, debug=True)
