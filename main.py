7963d7c88fe772d2c4d963906397019dc30f6ead
Dimitri Glazkov <dimitri@glazkov.com>
2023-01-07 18:52:43 -0800

    Use polymath library at polymath.glazkov.com

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
+import json
 import os
 import random
 import re
 import traceback
 
 import openai
+import urllib3
 from dotenv import load_dotenv
 from flask import Flask, jsonify, render_template, request
 from google.appengine.api import memcache, wrap_wsgi_app
 
-from ask_embeddings import ask, ask_start
+import polymath
 
 START_QUERY = "list some interesting key concepts, each on new line"
 LIST_QUERY = "list some interesting key concepts related to {concept}, each on new line"
 DESCRIBE_QUERY = "describe {concept}"
 MAX_ITEMS_PER_LIST = 7
-EMBEDDINGS_FILE = "embeddings/what-dimitri-learned.pkl"
+POLYMATH_SERVER = "polymath.glazkov.com"
 WANDERING_MEMORY = 60 * 60 * 2  # 2 hours, why not
 WANDERING_VARIETY = 5
+CONTEXT_TOKEN_COUNT = 1500
 
 app = Flask(__name__)
 app.wsgi_app = wrap_wsgi_app(app.wsgi_app)
@@ -47,8 +50,41 @@ def make_list(response):
     return [sanitize(line) for line in lines]
 
 
+def query_polymath_server(query_embedding, random, server):
+    http = urllib3.PoolManager()
+    fields = {
+        "version": polymath.CURRENT_VERSION,
+        "access_token": server,
+        "query_embedding_model": polymath.EMBEDDINGS_MODEL_ID,
+        "count": CONTEXT_TOKEN_COUNT
+    }
+    if random:
+        fields["sort"] = "random"
+    else:
+        fields["query_embedding"] = query_embedding
+    response = http.request(
+        'POST', server, fields=fields).data
+    obj = json.loads(response)
+    if 'error' in obj:
+        error = obj['error']
+        raise Exception(f"Server returned an error: {error}")
+    return polymath.Library(data=obj)
+
+
+def ask(query, random=False):
+    query_vector = None if random else polymath.base64_from_vector(
+        polymath.get_embedding(query))
+
+    library = query_polymath_server(query_vector, random, POLYMATH_SERVER)
+    context = polymath.get_context_for_library(library)
+    sources = polymath.get_chunk_infos_for_library(library)
+    completion = polymath.get_completion_with_context(query, context)
+
+    return (completion, sources[:3])
+
+
 def ask_to_start(_):
-    (response, issues) = ask_start(START_QUERY, EMBEDDINGS_FILE)
+    (response, issues) = ask(START_QUERY, True)
     return jsonify({
         "list": make_list(response),
         "issues": issues
@@ -72,7 +108,7 @@ def start_sample():
 
 
 def ask_to_describe(concept):
-    (response, issues) = ask(DESCRIBE_QUERY.format(concept=concept), EMBEDDINGS_FILE)
+    (response, issues) = ask(DESCRIBE_QUERY.format(concept=concept))
     return jsonify({
         "text": response,
         "issues": issues
@@ -101,7 +137,7 @@ def describe_sample():
 
 
 def ask_to_list(concept):
-    (response, issues) = ask(LIST_QUERY.format(concept=concept), EMBEDDINGS_FILE)
+    (response, issues) = ask(LIST_QUERY.format(concept=concept))
     return jsonify({
         "list": make_list(response),
         "issues": issues
